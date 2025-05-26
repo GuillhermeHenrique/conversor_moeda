ed0933277b1e1434f8b6d5699bda1e6a3967b1b6
GuillhermeHenrique <piresguilherme003@gmail.com>
2025-05-26 19:45:55 -0300

    feat: implementa integração de APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,89 @@
+import { useEffect, useState } from "react";
+import { getConverter, getCountryFlag, getCoinCode } from "./api/requests";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [conversion, setConversion] = useState<number | null>(null);
+  const [flags, setFlags] = useState<string[]>([]);
+  const [coinCode, setCoinCode] = useState<string[]>([]);
+  const [coinPrimary, setCoinPrimary] = useState<string>("");
+  const [coinSecundary, setCoinSecundary] = useState<string>("");
+
+  const handleConversion = async (from: string, to: string) => {
+    try {
+      const conversion = await getConverter(from, to);
+
+      setConversion(conversion);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const handleCoinCode = async () => {
+      try {
+        const coinData = await getCoinCode();
+
+        const key = Object.keys(coinData).slice(0, 30);
+
+        setCoinCode(key);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    handleCoinCode();
+  }, []);
+
+  useEffect(() => {
+    const handleFlags = async () => {
+      try {
+        if (coinCode.length > 0) {
+          const countryFlags = await Promise.all(
+            coinCode.map((code) => getCountryFlag(code))
+          );
+
+          setFlags(countryFlags);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    handleFlags();
+  }, [coinCode]);
+
+  return (
+    <div className="App">
+      <select onChange={(e) => setCoinPrimary(e.target.value)}>
+        {coinCode?.map((coin) => (
+          <option key={coin} value={coin}>
+            {coin}
+          </option>
+        ))}
+      </select>
+      <select onChange={(e) => setCoinSecundary(e.target.value)}>
+        {coinCode?.map((coin) => (
+          <option key={coin} value={coin}>
+            {coin}
+          </option>
+        ))}
+      </select>
+      <button
+        type="submit"
+        onClick={() => handleConversion(coinPrimary, coinSecundary)}
+      >
+        Calcular
+      </button>
+      <h2>{conversion}</h2>
+      <div>
+        {flags?.map((flag, index) => (
+          <img key={index} src={flag} alt="flag" />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/requests.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+// import { useFlagResponse } from "../hooks/useFlags";
+
+export const getConverter = async (from: string, to: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`
+    );
+
+    return response.data.rates[to];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCountryFlag = async (countryCoin: string) => {
+  try {
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/currency/${countryCoin}`
+    );
+
+    // useFlagResponse(countryCoin);
+
+    return response.data[0].flags.png;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getCoinCode = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.frankfurter.dev/v1/latest?base=ZAR`
+    );
+
+    return response.data.rates;
+  } catch (error) {
+    console.log(error);
+  }
+};
